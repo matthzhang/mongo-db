ceca06353c2e2774cf7b8344b77d05e016c63936
matthzhang <matthewzhang0226@yahoo.com>
2023-06-28 15:25:27 -0500

    completed tutorial, commented cmd args in docker-compose file

---
+++ students.js
@@ -0,0 +1,14 @@
+student = {
+    "firstName": "Matt",
+    "lastName": "Zhang",
+    "email": "zhan7342@umn.edu",
+    "gender": "F",
+    "country": "USA",
+    "isStudentActive": false,
+    "favoriteSubjects": [
+        "math",
+        "english",
+        "IT"
+    ],
+    "totalSpentInBooks": 0.00
+}
